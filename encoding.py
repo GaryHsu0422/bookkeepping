d7e67ddfab45db8e00a42c139ab679c7572909ef
Gary Hsu <garyhsu0422@gmaill.com>
2021-08-26 11:45:04 +0800

    added_encoding_'utf-8'_version

---
+++ encoding.py
@@ -0,0 +1,15 @@
+products = []
+while True: 
+	name = input('Please enter the name of the product: ')
+	if name == 'q':
+		break
+	price = input('Please enter the price of the product: ')
+	products.append([name, price])
+print(products)
+
+for p in products:
+	print('The price of', p[0], ' is', p[1])
+with open('products.csv', 'w', encoding = 'utf-8') as f:
+	f.write('Product, Price\n')
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
